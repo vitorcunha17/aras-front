187d8bf492fed5d800be0d052b856278d5348a93
vitorcunha17 <vitorribeirocunha@hotmail.com>
2020-08-01 15:33:34 -0300

    Removing useForm from antd utils

--- src/pages/Landing/Header/LoginModal/index.tsx
+++ src/pages/Landing/Header/LoginModal/index.tsx
@@ -1,6 +1,6 @@
 import React, { ForwardRefRenderFunction, forwardRef, useState, useImperativeHandle, useEffect, useContext } from 'react'
 import { Modal, Form, Input } from 'antd'
-import { useForm } from 'antd/lib/form/util';
+// import { useForm } from 'antd/lib/form/util';
 import logoTransparente from "../../../../images/logo_circular.png";
 import { useMutation } from 'react-apollo';
 import { Credentials, SigninResponse, SigninVariables } from './interfaces';
@@ -15,7 +15,7 @@ const LoginModal: ForwardRefRenderFunction<{ open(): void }> = ({ }, ref) => {
 
     const { push } = useHistory()
 
-    const [form] = useForm()
+    // const [form] = useForm()
 
     const [signin, { data }] = useMutation<SigninResponse, SigninVariables>(SIGNIN)
 
@@ -66,9 +66,9 @@ const LoginModal: ForwardRefRenderFunction<{ open(): void }> = ({ }, ref) => {
             okText="Login"
             cancelText="Cancelar"
             onCancel={close}
-            onOk={() => form.submit()}
+            // onOk={() => form.submit()}
         >
-            <Form layout="vertical" form={form} onFinish={onFinish}>
+            <Form layout="vertical" onFinish={onFinish}>
                 <Form.Item
                     label="Email"
                     name="email"
